cc1c246720835a4250a31ef4fe2f429f78cdda1d
Ali Talebi <sudofrost@gmail.com>
2024-02-02 12:50:52 +0330

    Implement and test hash table data structure

---
+++ src/data-structures/hash-tables/HashTable.ts
@@ -0,0 +1,46 @@
+import { LinkedList } from '@/data-structures/linked-list/LinkedList';
+
+export class HashTable<TKey extends number | string, TValue> {
+    private static capability = 10
+
+    private _table: LinkedList<[TKey, TValue]>[]
+    private _keys: LinkedList<TKey>
+
+    constructor() {
+        this._table = []
+        this._keys = new LinkedList()
+    }
+
+    private calculateHash(key: TKey) {
+        let hash: number
+        if (typeof key === "number") {
+            hash = key % HashTable.capability
+        } else {
+            hash = key
+                .split('')
+                .map(c => c.charCodeAt(0) % HashTable.capability)
+                .reduce((s, c) => s + c) % HashTable.capability
+        }
+        return hash
+    }
+
+    put(key: TKey, value: TValue) {
+        const hash = this.calculateHash(key)
+        if (!this._table[hash]) {
+            this._table[hash] = new LinkedList()
+        }
+        this._table[hash].append([key, value])
+        this._keys.append(key)
+    }
+
+    get(key: TKey) {
+        const hash = this.calculateHash(key)
+        if (!this._table[hash]) return null
+        const index = this._table[hash].indexOf((v) => v[0] === key)
+        return index >= 0 ? this._table[hash].get(index)![1] : null
+    }
+
+    keys() {
+        return this._keys.toArray()
+    }
+}
---
+++ tests/data-structures/hash-table/hash-table.test.ts
@@ -0,0 +1,33 @@
+import { HashTable } from '@/data-structures/hash-tables/HashTable';
+import { describe, expect, test } from 'bun:test';
+
+describe('HashTable', () => {
+    test('put', () => {
+        const hashTable = new HashTable<string, number>()
+        hashTable.put('a', 1)
+        hashTable.put('b', 2)
+        hashTable.put('c', 3)
+        expect(hashTable.keys()).toEqual(['a', 'b', 'c'])
+    })
+
+    test('get', () => {
+        const hashTable = new HashTable<string, number>()
+        hashTable.put('a', 1)
+        hashTable.put('b', 2)
+        hashTable.put('c', 3)
+        expect(hashTable.get('a')).toBe(1)
+        expect(hashTable.get('b')).toBe(2)
+        expect(hashTable.get('c')).toBe(3)
+        expect(hashTable.get('d')).toBe(null)
+    })
+
+    test('keys', () => {
+        const hashTable = new HashTable<string, number>()
+        hashTable.put('a', 1)
+        hashTable.put('b', 2)
+        hashTable.put('c', 3)
+        expect(hashTable.keys()).toEqual(['a', 'b', 'c'])
+        hashTable.put('d', 4)
+        expect(hashTable.keys()).toEqual(['a', 'b', 'c', 'd'])
+    })
+})
