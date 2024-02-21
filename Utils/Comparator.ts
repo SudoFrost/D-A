2176cdd118dac18f3f25adb08cee0ff530727982
Ali Talebi <sudofrost@gmail.com>
2024-02-21 22:49:42 +0330

    Refactor: Update Comparator class to expose cmp directly and fix related errors

--- Utils/Comparator.ts
+++ Utils/Comparator.ts
@@ -2,12 +2,8 @@ export type CompareFunc<T> = (a: T, b: T) => number
 
 export class Comparator<T> {
     constructor(
-        private _compare: CompareFunc<T> = (a, b) => a == b ? 0 : a > b ? 1 : -1
+        public cmp: CompareFunc<T> = (a, b) => a == b ? 0 : a > b ? 1 : -1
     ) { }
-
-    cmp(a: T, b: T): number {
-        return this._compare(a, b)
-    }
 
     eq(a: T, b: T): boolean {
         return this.cmp(a, b) === 0
